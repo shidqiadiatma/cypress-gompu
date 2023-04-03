7233cd75c252ddc0dc35e96cda99930ea44e7b5a
hipstertester <shidqiadiatma12@gmail.com>
2023-04-03 21:45:48 +0700

    deleted mochaweshome

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,14 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportFilename: 'iteration-3',
-    quiet: true,
-  },
   e2e: {
+    
     baseUrl: "https://gompu.netlify.app/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  pageLoadTimeout: 10000,
+  pageLoadTimeout: 5000,
   viewportWidth: 1280,
   viewportHeight: 720,
 });
