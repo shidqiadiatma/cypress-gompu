9218738a10dd656c659b9add01be5d95fe6d5e36
hipstertester <shidqiadiatma12@gmail.com>
2023-04-03 15:41:50 +0700

    refactor code final cypress

--- cypress/support/pageObject/register-page.js
+++ cypress/support/pageObject/register-page.js
@@ -2,21 +2,16 @@ class register{
     go_to_register_page() {
         cy.get('.text-center > .text-primary-color').click()
     }
-    input_fullname(nama_lengkap){
+    do_register(nama_lengkap, email_pengguna, password_pengguna){
         cy.get('[type="text"]').type(nama_lengkap)
-    }
-    input_email(email_pengguna){
         cy.get('[type="email"]').type(email_pengguna)
-    }
-    input_password(password_pengguna){
         cy.get('.grid > .flex > .w-full').type(password_pengguna)
     }
-    clickButton_registrasi(){
+    click_btnRegistrasi(){
         cy.get('.btn-primary').click()
     }
     verify_failed_register_using_registered_email(){
         cy.get('#swal2-title').should('have.text', 'Email has already been taken')
-
     }
     verify_failed_register_using_invalid_format_password(){
         cy.get('.text-red-400').should('have.text', 'Kata Sandi tidak valid!')
--- cypress/support/pageObject/updateProfile-page.js
+++ cypress/support/pageObject/updateProfile-page.js
@@ -1,50 +1,31 @@
 
 class updateProfile{
-    go_to_profile_page(){
+    go_to_UpdateProfile_page(){
         cy.get('.gap-14 > [href="/profile"]').click()
-    }
-    go_to_updateProfile_page(){
         cy.get(':nth-child(1) > .flex > .flex-auto').click()
     }
-    clear_fullName(){
+    do_clearAllDataProfile(){
         cy.xpath('//*[@id="__layout"]/div/div/form/input[1]').type(' ').clear()
-    }
-    clear_email(){
         cy.xpath('//*[@id="__layout"]/div/div/form/input[2]').type(' ').clear()
-    }
-    clear_noHandphone(){
         cy.xpath('//*[@id="__layout"]/div/div/form/div/input').type(' ').clear()
-    }
-    clear_tanggalLahir(){
         cy.xpath('//*[@id="__layout"]/div/div/form/input[3]').click('').clear()
-    }
-    clear_kota(){
         cy.xpath('//*[@id="__layout"]/div/div/form/input[4]').type(' ').clear()
     }
-    clickSave_button(){
-        cy.get('.btn-primary').click()
+    do_updateAllDataProfile(fullNameUpdate, emailUpdate, noPhoneUpdate, dateUpdate, cityUpdate){
+        cy.xpath('//*[@id="__layout"]/div/div/form/input[1]').clear().type(fullNameUpdate)
+        cy.xpath('//*[@id="__layout"]/div/div/form/input[2]').clear().type(emailUpdate)
+        cy.xpath('//*[@id="__layout"]/div/div/form/div/input').clear().type(noPhoneUpdate)
+        cy.xpath('//*[@id="__layout"]/div/div/form/input[3]').clear().click(dateUpdate)
+        cy.xpath('//*[@id="__layout"]/div/div/form/input[4]').clear().type(cityUpdate)
     }
     verify_failed_updateProfile(){
         cy.get('#swal2-title').should('have.text', "Full name can't be blank")
     }
-    fill_fullName(fullNameUpdate){
-        cy.xpath('//*[@id="__layout"]/div/div/form/input[1]').clear().type(fullNameUpdate)
-    }
-    fill_email(emailUpdate){
-        cy.xpath('//*[@id="__layout"]/div/div/form/input[2]').clear().type(emailUpdate)
-    }
-    fill_noHandphone(noPhoneUpdate){
-        cy.xpath('//*[@id="__layout"]/div/div/form/div/input').clear().type(noPhoneUpdate)
-    }
-    fill_tanggalLahir(dateUpdate){
-        cy.xpath('//*[@id="__layout"]/div/div/form/input[3]').clear().click(dateUpdate)
-    }
-    fill_kota(cityUpdate){
-        cy.xpath('//*[@id="__layout"]/div/div/form/input[4]').clear().type(cityUpdate)
-    }
-
     verify_success_updateProfile(){
         cy.get('#swal2-title').should('have.text', "Detail user berhasil diperbarui")
+    }
+    clickSave_button(){
+        cy.get('.btn-primary').click()
     }
 }
 export default updateProfile
