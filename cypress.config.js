ff8d0255c77c9064e7420377a05a33549ca05939
hipstertester <shidqiadiatma12@gmail.com>
2023-04-03 20:47:58 +0700

    selesai

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportFilename: 'iteration-3',
+    quiet: true,
+  },
   e2e: {
-    
     baseUrl: "https://gompu.netlify.app/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
